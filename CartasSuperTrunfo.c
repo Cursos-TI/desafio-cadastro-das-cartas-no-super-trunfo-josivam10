abd79f8059b8cbcf848e0e68b682e3927bad3a78
josivam10 <josivam10@gmail.com>
2025-03-30 14:14:34 +0000

    COMECANDO O JOGO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 int main(){
 
-    char estado1;
-    char codigodacarta1;
-    char nomedacidade1;
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosT1;
+    char estado3;
+    char codigodacarta3;
+    char capital3;
+    int populacao3;
+    float area3;
+    float pib3;
+    int pontosT3;
 
-        char estado2;
-        char codigodacarta2;
-        char nomedacidade2;
-        int populacao2;
-        float area2;
-        float pib2;
-        int pontosT2;
+    char estado2;
+    char codigodacarta2;
+    char capital2;
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosT2;
 
-   printf ("BEM VINDO AO JOGO \n");
+printf ("BEM VINDO AO JOGO \n");
+printf ("EXEMPLO DO JOGO! \n");
+printf("Carta 1 \n");
+printf("Estado: Rio de Janeiro \n");
+printf("Código: 021 \n");
+printf("Capital: Rio de Janeiro \n");
+printf("População: 6748000 \n");
+printf("Área: 1200.25 km² \n");
+printf("PIB: 300.50 bilhões de reais \n");
+printf("Número de Pontos Turísticos: 30 \n");
+printf("VAMOS COMECAO JOGO \n")
+
+
+
 
     return 0;
 }
