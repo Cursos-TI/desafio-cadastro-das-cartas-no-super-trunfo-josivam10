bb286bac5c6af86201288a7805ec8407e6702d1f
josivam10 <josivam10@gmail.com>
2025-04-15 16:01:07 +0000

    continuacao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 int main(){
 
-    char estado3;
-    char codigodacarta3;
-    char capital3;
-    int populacao3;
-    float area3;
-    float pib3;
-    int pontosT3;
-
-    char estado2;
-    char codigodacarta2;
-    char capital2;
-    int populacao2;
-    float area2;
-    float pib2;
-    int pontosT2;
+    char estado, capital;
+    int populacao, ddd;
+    float area;
+    float pib;
+    int pontosT;
 
 printf ("BEM VINDO AO JOGO \n");
 printf ("EXEMPLO DO JOGO! \n");
@@ -27,10 +17,31 @@ printf("População: 6748000 \n");
 printf("Área: 1200.25 km² \n");
 printf("PIB: 300.50 bilhões de reais \n");
 printf("Número de Pontos Turísticos: 30 \n");
-printf("VAMOS COMECAO JOGO \n")
-
-
-
+printf("____________________________");
+    printf("VAMOS COMEÇAR O JOGO \n");
+    printf("Digite a carta 2 \n");
+    printf("Qual o nome do estado: \n");
+    scanf("%s",&estado);
+    printf("Qual o DDD do estado: \n");
+    scanf("%d",&ddd);
+    printf("Digite a capital: \n");
+    scanf("%s",&capital);
+    printf("Digite a populacao: \n");
+    scanf("%d",&populacao);
+    printf("Digite a area do estado: \n");
+    scanf("%f",&area);
+    printf("Digite o pib: \n");
+    scanf("%f",&pib);
+    printf("Digite a quantidade de pontos turisticos: \n");
+    scanf("%d",&pontosT);
+    printf("AQUI ESTA SUA CARTA!");
+    printf("Estado: %s \n",estado);
+    printf("Código: %d \n",ddd);
+    printf("Capital: %s \n",capital);
+    printf("População: %d \n",populacao);
+    printf("Área: %.2f \n",area);
+    printf("PIB: %.2f bilhões de reais \n",pib);
+    printf("Número de Pontos Turísticos: %d \n",pontosT);
 
     return 0;
 }
